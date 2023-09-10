179f17198b17b78a12bea04d3d35f4c1be436366
dakshvasudev <vasudevdaksh@gmail.com>
2023-09-10 14:59:50 +0530

    google sign in setup for controller

--- lib/pages/frames/signin/controller.dart
+++ lib/pages/frames/signin/controller.dart
@@ -1,7 +1,45 @@
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:x_connect/common/entities/entities.dart';
 import 'package:x_connect/pages/frames/signin/state.dart';
 
 class SignInController extends GetxController {
   SignInController();
   final state = SignInState();
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['openid']);
+
+  Future<void> handleSignIn(String type) async {
+    //1: email, 2:google, 3: facebook , 4:apple ,5:phone
+    try {
+      if (type == 'phone number') {
+        if (kDebugMode) {
+          print('------you are logging in with phone number------');
+        }
+      } else if (type == 'google') {
+        var user = await _googleSignIn.signIn();
+        if (user != null) {
+          String? displayName = user.displayName;
+          String email = user.email;
+          String id = user.id;
+          String photoUrl = user.photoUrl ?? "assets/icons/google.png";
+          LoginRequestEntity loginPanelListRequestEntity = LoginRequestEntity();
+          loginPanelListRequestEntity.email = email;
+          loginPanelListRequestEntity.name = displayName;
+          loginPanelListRequestEntity.avatar = photoUrl;
+          loginPanelListRequestEntity.open_id = id;
+          loginPanelListRequestEntity.type = 2;
+        }
+      } else {
+        if (kDebugMode) {
+          print('------login type not defined------');
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('-----error with login $e-----');
+      }
+    }
+  }
 }
