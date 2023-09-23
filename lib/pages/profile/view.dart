076de0e097b13befd5dfb82505bb25dfaa78d143
dakshvasudev <vasudevdaksh@gmail.com>
2023-09-23 12:37:23 +0530

    11.44 working

--- lib/pages/profile/view.dart
+++ lib/pages/profile/view.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:x_connect/common/values/colors.dart';
 import 'package:x_connect/pages/frames/welcome/index.dart';
 
 class ProfilePage extends GetView<WelcomeController> {
   const ProfilePage({super.key});
 
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: const Text(
+        'Profile',
+        style: TextStyle(
+          color: AppColors.primaryText,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [],
+        ),
+      ),
+    );
   }
 }
