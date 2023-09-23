bbe06d231dc89e430b0c5475024d36d89b29ebc3
dakshvasudev <vasudevdaksh@gmail.com>
2023-09-23 12:57:12 +0530

    11.48 working

--- lib/pages/profile/view.dart
+++ lib/pages/profile/view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:x_connect/common/values/colors.dart';
 import 'package:x_connect/pages/frames/welcome/index.dart';
@@ -111,6 +112,56 @@ class ProfilePage extends GetView<WelcomeController> {
         });
   }
 
+  Widget _buildLogoutBtn() {
+    return GestureDetector(
+        child: Container(
+          width: 295,
+          height: 44,
+          margin: EdgeInsets.only(top: 0, bottom: 30),
+          padding: EdgeInsets.all(0),
+          decoration: BoxDecoration(
+            color: AppColors.primarySecondaryElementText,
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: Offset(0, 1), // changes position of shadow
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Text(
+                  "Logout",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: AppColors.primaryElementText,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        onTap: () {
+          Get.defaultDialog(
+            title: "Are you sure to log out?",
+            content: Container(),
+            onConfirm: () {
+              // controller.goLogout();
+            },
+            onCancel: () {},
+            textConfirm: "Confirm",
+            textCancel: "Cancel",
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -124,6 +175,7 @@ class ProfilePage extends GetView<WelcomeController> {
                   children: [
                     _buildProfilePhoto(),
                     _buildCompleteBtn(),
+                    _buildLogoutBtn(),
                   ],
                 ),
               ),
