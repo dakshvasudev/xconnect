2d91e01d975c65c6abff917a4f7cefe9150684d5
dakshvasudev <vasudevdaksh@gmail.com>
2023-10-24 00:31:27 +0530

    18.74 working

--- lib/pages/message/view.dart
+++ lib/pages/message/view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:x_connect/common/routes/names.dart';
 import 'package:x_connect/common/values/colors.dart';
 import 'package:x_connect/pages/message/controller.dart';
 
@@ -69,18 +71,44 @@ class MessagePage extends GetView<MessageController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(
-          children: [
-            CustomScrollView(
-              slivers: [
-                SliverAppBar(
-                  pinned: true,
-                  title: _headBar(MediaQuery.of(context).size.width),
-                ),
-              ],
-            )
-          ],
-        ),
+        child: Stack(children: [
+          CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                pinned: true,
+                title: _headBar(MediaQuery.of(context).size.width),
+              ),
+            ],
+          ),
+          Positioned(
+            right: 20,
+            bottom: 20,
+            height: 50,
+            width: 50,
+            child: GestureDetector(
+              child: Container(
+                  height: 50,
+                  width: 50,
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: AppColors.primaryElement,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        spreadRadius: 2,
+                        blurRadius: 2,
+                        offset: Offset(1, 1), // changes position of shadow
+                      ),
+                    ],
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                  ),
+                  child: Image.asset("assets/icons/contact.png")),
+              onTap: () {
+                Get.toNamed(AppRoutes.Contact);
+              },
+            ),
+          )
+        ]),
       ),
     );
   }
