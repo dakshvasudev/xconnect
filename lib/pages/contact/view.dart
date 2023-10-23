2e01b1581b2f8d3923f0ed2a6b95642cadaebeaa
dakshvasudev <vasudevdaksh@gmail.com>
2023-10-24 00:44:59 +0530

    20.77 working fine

--- lib/pages/contact/view.dart
+++ lib/pages/contact/view.dart
@@ -2,19 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:x_connect/common/values/values.dart';
 import 'package:x_connect/pages/contact/index.dart';
+import 'package:x_connect/pages/contact/widgets/contact_list.dart';
 
 class ContactPage extends GetView<ContactController> {
   const ContactPage({super.key});
+  AppBar _buildAppBar() {
+    return AppBar(
+        title: const Text(
+      "Contact",
+      style: TextStyle(
+        color: AppColors.primaryText,
+        fontSize: 16,
+        fontWeight: FontWeight.normal,
+      ),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primaryElement,
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Center(child: Text('Hi')),
-      ),
+      appBar: _buildAppBar(),
+      body: ContactList(),
     );
   }
 }
