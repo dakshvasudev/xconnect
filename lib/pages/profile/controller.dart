d5d399d5f60b9f5bdc9bf32175e3bf4c0f79fcc3
dakshvasudev <vasudevdaksh@gmail.com>
2023-11-19 01:38:17 +0530

    profile ui

--- lib/pages/profile/controller.dart
+++ lib/pages/profile/controller.dart
@@ -3,8 +3,13 @@ import 'package:flutter_cache_manager/file.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:x_connect/common/apis/user.dart';
+import 'package:x_connect/common/entities/user.dart';
 import 'package:x_connect/common/store/user.dart';
+import 'package:x_connect/common/widgets/toast.dart';
 import 'package:x_connect/pages/profile/state.dart';
+
+import '../../common/apis/apis.dart';
 
 class ProfileController extends GetxController {
   ProfileController();
@@ -27,6 +32,72 @@ class ProfileController extends GetxController {
       if (state.profile_detail.value.description != null) {
         DescriptionEditingController?.text = state.profile_detail.value.description!;
       }
+    }
+  }
+
+  goSave() async {
+    if (state.profile_detail.value.name == null || state.profile_detail.value.name!.isEmpty) {
+      toastInfo(msg: "name not empty!");
+      return;
+    }
+    if (state.profile_detail.value.description == null || state.profile_detail.value.description!.isEmpty) {
+      toastInfo(msg: "description not empty!");
+      return;
+    }
+    if (state.profile_detail.value.avatar == null || state.profile_detail.value.avatar!.isEmpty) {
+      toastInfo(msg: "avatar not empty!");
+      return;
+    }
+
+    LoginRequestEntity updateProfileRequestEntity = new LoginRequestEntity();
+    var userItem = state.profile_detail.value;
+    updateProfileRequestEntity.avatar = userItem.avatar;
+    updateProfileRequestEntity.name = userItem.name;
+    updateProfileRequestEntity.description = userItem.description;
+    updateProfileRequestEntity.online = userItem.online;
+
+    var result = await UserAPI.UpdateProfile(params: updateProfileRequestEntity);
+    print(result.code);
+    print(result.msg);
+    if (result.code == 0) {
+      UserItem userItem = state.profile_detail.value;
+      await UserStore.to.saveProfile(userItem);
+      Get.back(result: "finish");
+    }
+  }
+
+  Future imgFromGallery() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      // _photo = File(pickedFile.path);
+      uploadFile();
+    } else {
+      print('No image selected.');
+    }
+  }
+
+  Future imgFromCamera() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      // _photo = File(pickedFile.path);
+      uploadFile();
+    } else {
+      print('No image selected.');
+    }
+  }
+
+  Future uploadFile() async {
+    // if (_photo == null) return;
+    // print(_photo);
+    var result = await ChatAPI.upload_img(file: _photo);
+    print(result.data);
+    if (result.code == 0) {
+      state.profile_detail.value.avatar = result.data;
+      state.profile_detail.refresh();
+    } else {
+      toastInfo(msg: "image error");
     }
   }
 
--- lib/pages/profile/view.dart
+++ lib/pages/profile/view.dart
@@ -20,6 +20,238 @@ class ProfilePage extends GetView<ProfileController> {
     );
   }
 
+  Widget _buildNameInput() {
+    return Container(
+        width: 295,
+        height: 44,
+        margin: const EdgeInsets.only(bottom: 20, top: 0),
+        padding: const EdgeInsets.all(0),
+        decoration: BoxDecoration(
+          color: AppColors.primaryBackground,
+          borderRadius: const BorderRadius.all(Radius.circular(5)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: const Offset(0, 1), // changes position of shadow
+            ),
+          ],
+        ),
+        child: TextField(
+          keyboardType: TextInputType.multiline,
+          controller: controller.NameEditingController,
+          decoration: InputDecoration(
+            hintText: controller.state.profile_detail.value.name,
+            contentPadding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+            border: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            disabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            hintStyle: const TextStyle(
+              color: AppColors.primarySecondaryElementText,
+            ),
+          ),
+          style: const TextStyle(
+            color: AppColors.primaryText,
+            fontFamily: "Avenir",
+            fontWeight: FontWeight.normal,
+            fontSize: 14,
+          ),
+          maxLines: 1,
+          onChanged: (value) {
+            controller.state.profile_detail.value.name = value;
+          },
+          autocorrect: false,
+          // 自动纠正
+          obscureText: false, // 隐藏输入内容, 密码框
+        ));
+  }
+
+  Widget _buildDescripeInput() {
+    return Container(
+        width: 295,
+        height: 44,
+        margin: const EdgeInsets.only(bottom: 20, top: 0),
+        padding: const EdgeInsets.all(0),
+        decoration: BoxDecoration(
+          color: AppColors.primaryBackground,
+          borderRadius: const BorderRadius.all(Radius.circular(5)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: const Offset(0, 1), // changes position of shadow
+            ),
+          ],
+        ),
+        child: TextField(
+          keyboardType: TextInputType.multiline,
+          controller: controller.DescriptionEditingController,
+          decoration: InputDecoration(
+            hintText: controller.state.profile_detail.value.description == null
+                ? "Enter a description"
+                : controller.state.profile_detail.value.description,
+            contentPadding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+            border: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            disabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            hintStyle: const TextStyle(
+              color: AppColors.primarySecondaryElementText,
+            ),
+          ),
+          style: const TextStyle(
+            color: AppColors.primaryText,
+            fontFamily: "Avenir",
+            fontWeight: FontWeight.normal,
+            fontSize: 14,
+          ),
+          maxLines: 1,
+          onChanged: (value) {
+            controller.state.profile_detail.value.description = value;
+          },
+          autocorrect: false,
+          // 自动纠正
+          obscureText: false, // 隐藏输入内容, 密码框
+        ));
+  }
+
+  Widget _buildSeleteStatusInput() {
+    return Container(
+        width: 295,
+        height: 44,
+        margin: const EdgeInsets.only(bottom: 20, top: 0),
+        padding: const EdgeInsets.all(0),
+        decoration: BoxDecoration(
+          color: AppColors.primaryBackground,
+          borderRadius: const BorderRadius.all(Radius.circular(5)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: const Offset(0, 1), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: 12,
+              height: 1,
+              margin: const EdgeInsets.only(left: 15),
+              decoration: BoxDecoration(
+                color: controller.state.profile_detail.value.online == 1
+                    ? AppColors.primaryElementStatus
+                    : AppColors.primarySecondaryElementText,
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+              ),
+            ),
+            SizedBox(
+                width: 200,
+                height: 44,
+                child: TextField(
+                  keyboardType: TextInputType.multiline,
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    hintText: controller.state.profile_detail.value.online == 1 ? "Online" : "Offline",
+                    hintStyle: const TextStyle(color: AppColors.primarySecondaryElementText),
+                    contentPadding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                    border: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                      ),
+                    ),
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                      ),
+                    ),
+                    disabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                      ),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.transparent,
+                      ),
+                    ),
+                  ),
+                  style: const TextStyle(
+                    color: AppColors.primaryText,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14,
+                  ),
+                  maxLines: 1,
+                  autocorrect: false,
+                  // 自动纠正
+                  obscureText: false, // 隐藏输入内容, 密码框
+                )),
+            Container(
+              width: 50,
+              height: 30,
+              padding: const EdgeInsets.only(left: 0),
+              decoration: const BoxDecoration(
+                  border: Border(
+                left: BorderSide(width: 2, color: AppColors.primarySecondaryBackground),
+              )),
+              child: DropdownButtonHideUnderline(
+                  child: DropdownButton(
+                      icon: const Icon(Icons.arrow_drop_down),
+                      iconSize: 35,
+                      iconEnabledColor: AppColors.primarySecondaryElementText,
+                      hint: const Text(''),
+                      elevation: 0,
+                      isExpanded: true,
+                      // underline:Container(),
+                      items: const [
+                        DropdownMenuItem(child: Text('Online'), value: 1),
+                        DropdownMenuItem(child: Text('Offline'), value: 2),
+                      ],
+                      onChanged: (value) {
+                        controller.state.profile_detail.value.online = value;
+                        controller.state.profile_detail.refresh();
+                      })),
+            )
+          ],
+        ));
+  }
+
   Widget _buildProfilePhoto() {
     return Stack(
       children: [
@@ -176,6 +408,86 @@ class ProfilePage extends GetView<ProfileController> {
         });
   }
 
+  Widget _buildLogo(BuildContext context) {
+    return Stack(alignment: Alignment.center, children: [
+      Container(
+        width: 120,
+        height: 120,
+        margin: const EdgeInsets.only(top: 0, bottom: 50),
+        decoration: BoxDecoration(
+          color: AppColors.primarySecondaryBackground,
+          borderRadius: const BorderRadius.all(Radius.circular(60)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: const Offset(0, 1), // changes position of shadow
+            ),
+          ],
+        ),
+        child: CachedNetworkImage(
+            imageUrl: controller.state.profile_detail.value.avatar!,
+            height: 120,
+            width: 120,
+            imageBuilder: (context, imageProvider) => Container(
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(60)),
+                    image: DecorationImage(image: imageProvider, fit: BoxFit.fill),
+                  ),
+                )),
+      ),
+      Positioned(
+          bottom: 50,
+          right: 0,
+          height: 35,
+          child: GestureDetector(
+              child: Container(
+                height: 35,
+                width: 35,
+                padding: const EdgeInsets.all(7),
+                decoration: const BoxDecoration(
+                  color: AppColors.primaryElement,
+                  borderRadius: BorderRadius.all(Radius.circular(40)),
+                ),
+                child: Image.asset(
+                  "assets/icons/edit.png",
+                ),
+              ),
+              onTap: () {
+                _showPicker(context);
+              }))
+    ]);
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                    leading: const Icon(Icons.photo_library),
+                    title: const Text('Gallery'),
+                    onTap: () {
+                      controller.imgFromGallery();
+                      Get.back();
+                    }),
+                ListTile(
+                  leading: const Icon(Icons.photo_camera),
+                  title: const Text('Camera'),
+                  onTap: () {
+                    controller.imgFromCamera();
+                    Get.back();
+                  },
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -184,14 +496,15 @@ class ProfilePage extends GetView<ProfileController> {
         child: CustomScrollView(
           slivers: [
             SliverToBoxAdapter(
-              child: Container(
-                child: Column(
-                  children: [
-                    _buildProfilePhoto(),
-                    _buildCompleteBtn(),
-                    _buildLogoutBtn(),
-                  ],
-                ),
+              child: Column(
+                children: [
+                  _buildLogo(context),
+                  _buildNameInput(),
+                  _buildDescripeInput(),
+                  _buildSeleteStatusInput(),
+                  _buildCompleteBtn(),
+                  _buildLogoutBtn()
+                ],
               ),
             )
           ],
